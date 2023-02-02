2ea3b17c818e49f2827e0bf562bcd719dc126e67
Chunsong Ma <cm641@duke.edu>
2023-02-02 14:07:18 -0500

    2

--- chunsong-ma-533/app.py
+++ chunsong-ma-533/app.py
@@ -3,18 +3,31 @@ import refinitiv.dataplatform.eikon as ek
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from datetime import date
 import plotly.express as px
 import os
 
 ek.set_app_key(os.getenv('EIKON_API'))
 
-dt_prc_div_splt = pd.read_csv('unadjusted_price_history.csv')
+#dt_prc_div_splt = pd.read_csv('unadjusted_price_history.csv')
 
 app = Dash(__name__)
 app.layout = html.Div([
     html.Div([
+        html.A(' Benchmark: '),
         dcc.Input(id = 'benchmark-id', type = 'text', value="IVV"),
-        dcc.Input(id = 'asset-id', type = 'text', value="AAPL.O")
+        html.A(' Asset: '),
+        dcc.Input(id = 'asset-id', type = 'text', value="AAPL.O"),
+        html.A(' Date Range: '),
+        dcc.DatePickerRange(
+            id='date-picker-range',
+            min_date_allowed=date(2017, 1, 1),
+            max_date_allowed=datetime.today(),
+            initial_visible_month=date(2023, 1, 1),
+            start_date=date(2017, 1, 1),
+            end_date=datetime.today()
+        ),
+        html.Div(id='output-container-date-picker-range')
     ]),
     html.Button('QUERY Refinitiv', id = 'run-query', n_clicks = 0),
     html.H2('Raw Data from Refinitiv'),
@@ -29,19 +42,95 @@ app.layout = html.Div([
         page_action='none',
         style_table={'height': '300px', 'overflowY': 'auto'}
     ),
+
+
+    html.Div([
+        html.A(' Benchmark: '),
+        dcc.Input(id = 'benchmark-id-1', type = 'text', value="IVV"),
+        html.A(' Asset: '),
+        dcc.Input(id = 'asset-id-1', type = 'text', value="AAPL.O"),
+        html.A(' Date Range: '),
+        dcc.DatePickerRange(
+            id='date-picker-range-1',
+            min_date_allowed=date(2017, 1, 1),
+            max_date_allowed=datetime.today(),
+            initial_visible_month=date(2023, 1, 1),
+            start_date=date(2017, 1, 1),
+            end_date=datetime.today()
+        ),
+        html.Div(id='output-container-date-picker-range-1')
+    ]),
+    html.Button('QUERY Refinitiv', id = 'run-query-1', n_clicks = 0),
+    html.H2('Raw Data from Refinitiv'),
+    dash_table.DataTable(
+        id = "history-tbl-1",
+        page_action='none',
+        style_table={'height': '300px', 'overflowY': 'auto'}
+    ),
+    html.H2('Historical Returns'),
+    dash_table.DataTable(
+        id = "returns-tbl-1",
+        page_action='none',
+        style_table={'height': '300px', 'overflowY': 'auto'}
+    ),
     html.H2('Alpha & Beta Scatter Plot'),
     dcc.Graph(id="ab-plot"),
-    html.P(id='summary-text', children="")
+    #html.P(id='summary-text', children=""),
+
+    html.H2('Alpha_Beta'),
+    html.P(id='alpha_beta_output'),
 ])
+
+"""
+@app.callback(
+    Output('output-container-date-picker-range', 'children'),
+    Input('date-picker-range', 'start_date'),
+    Input('date-picker-range', 'end_date'))
+def update_output(start_date, end_date):
+    string_prefix = 'You have selected: '
+    if start_date is not None:
+        start_date_object = date.fromisoformat(start_date)
+        start_date_string = start_date_object.strftime('%B %d, %Y')
+        string_prefix = string_prefix + 'Start Date: ' + start_date_string + ' | '
+    if end_date is not None:
+        end_date_object = date.fromisoformat(end_date)
+        end_date_string = end_date_object.strftime('%B %d, %Y')
+        string_prefix = string_prefix + 'End Date: ' + end_date_string
+    if len(string_prefix) == len('You have selected: '):
+        return 'Select a date to see it displayed here'
+    else:
+        return string_prefix
+
+
+@app.callback(
+    Output('output-container-date-picker-range-1', 'children'),
+    Input('date-picker-range-1', 'start_date'),
+    Input('date-picker-range-1', 'end_date'))
+def update_output(start_date, end_date):
+    string_prefix = 'You have selected: '
+    if start_date is not None:
+        start_date_object = date.fromisoformat(start_date)
+        start_date_string = start_date_object.strftime('%B %d, %Y')
+        string_prefix = string_prefix + 'Start Date: ' + start_date_string + ' | '
+    if end_date is not None:
+        end_date_object = date.fromisoformat(end_date)
+        end_date_string = end_date_object.strftime('%B %d, %Y')
+        string_prefix = string_prefix + 'End Date: ' + end_date_string
+    if len(string_prefix) == len('You have selected: '):
+        return 'Select a date to see it displayed here'
+    else:
+        return string_prefix
+"""
 
 @app.callback(
     Output("history-tbl", "data"),
     Input("run-query", "n_clicks"),
-    [State('benchmark-id', 'value'), State('asset-id', 'value')],
+    [State('benchmark-id', 'value'), State('asset-id', 'value'),
+     State('date-picker-range', 'start_date'), State('date-picker-range', 'end_date')],
     prevent_initial_call=True
 )
-def query_refinitiv(n_clicks, benchmark_id, asset_id):
-    assets = [benchmark_id, asset_id]
+def query_refinitiv(n_clicks, benchmark_id, asset_id, start_date, end_date):
+    assets = [benchmark_id, asset_id, start_date, end_date]
     prices, prc_err = ek.get_data(
         instruments=assets,
         fields=[
@@ -52,8 +141,8 @@ def query_refinitiv(n_clicks, benchmark_id, asset_id):
             'TR.PriceCloseDate'
         ],
         parameters={
-            'SDate': '2017-01-01',
-            'EDate': datetime.now().strftime("%Y-%m-%d"),
+            'SDate': start_date,
+            'EDate': end_date,
             'Frq': 'D'
         }
     )
@@ -67,8 +156,8 @@ def query_refinitiv(n_clicks, benchmark_id, asset_id):
             'TR.DivPaymentType'
         ],
         parameters={
-            'SDate': '2017-01-01',
-            'EDate': datetime.now().strftime("%Y-%m-%d"),
+            'SDate': start_date,
+            'EDate': end_date,
             'Frq': 'D'
         }
     )
@@ -78,8 +167,8 @@ def query_refinitiv(n_clicks, benchmark_id, asset_id):
         fields=['TR.CAEffectiveDate', 'TR.CAAdjustmentFactor'],
         parameters={
             "CAEventType": "SSP",
-            'SDate': '2017-01-01',
-            'EDate': datetime.now().strftime("%Y-%m-%d"),
+            'SDate': start_date,
+            'EDate': end_date,
             'Frq': 'D'
         }
     )
@@ -175,9 +264,151 @@ def calculate_returns(history_tbl):
     )
 
 @app.callback(
+    Output("history-tbl-1", "data"),
+    Input("run-query-1", "n_clicks"),
+    [State('benchmark-id-1', 'value'), State('asset-id-1', 'value'),
+     State('date-picker-range-1', 'start_date'), State('date-picker-range-1', 'end_date')],
+    prevent_initial_call=True
+)
+def query_refinitiv(n_clicks, benchmark_id, asset_id, start_date, end_date):
+    assets = [benchmark_id, asset_id, start_date, end_date]
+    prices, prc_err = ek.get_data(
+        instruments=assets,
+        fields=[
+            'TR.OPENPRICE(Adjusted=0)',
+            'TR.HIGHPRICE(Adjusted=0)',
+            'TR.LOWPRICE(Adjusted=0)',
+            'TR.CLOSEPRICE(Adjusted=0)',
+            'TR.PriceCloseDate'
+        ],
+        parameters={
+            'SDate': start_date,
+            'EDate': end_date,
+            'Frq': 'D'
+        }
+    )
+
+    divs, div_err = ek.get_data(
+        instruments=assets,
+        fields=[
+            'TR.DivExDate',
+            'TR.DivUnadjustedGross',
+            'TR.DivType',
+            'TR.DivPaymentType'
+        ],
+        parameters={
+            'SDate': start_date,
+            'EDate': end_date,
+            'Frq': 'D'
+        }
+    )
+
+    splits, splits_err = ek.get_data(
+        instruments=assets,
+        fields=['TR.CAEffectiveDate', 'TR.CAAdjustmentFactor'],
+        parameters={
+            "CAEventType": "SSP",
+            'SDate': start_date,
+            'EDate': end_date,
+            'Frq': 'D'
+        }
+    )
+
+    prices.rename(
+        columns={
+            'Open Price': 'open',
+            'High Price': 'high',
+            'Low Price': 'low',
+            'Close Price': 'close'
+        },
+        inplace=True
+    )
+    prices.dropna(inplace=True)
+    prices['Date'] = pd.to_datetime(prices['Date']).dt.date
+
+    divs.rename(
+        columns={
+            'Dividend Ex Date': 'Date',
+            'Gross Dividend Amount': 'div_amt',
+            'Dividend Type': 'div_type',
+            'Dividend Payment Type': 'pay_type'
+        },
+        inplace=True
+    )
+    divs.dropna(inplace=True)
+    divs['Date'] = pd.to_datetime(divs['Date']).dt.date
+    divs = divs[(divs.Date.notnull()) & (divs.div_amt > 0)]
+
+    splits.rename(
+        columns={
+            'Capital Change Effective Date': 'Date',
+            'Adjustment Factor': 'split_rto'
+        },
+        inplace=True
+    )
+    splits.dropna(inplace=True)
+    splits['Date'] = pd.to_datetime(splits['Date']).dt.date
+
+    unadjusted_price_history = pd.merge(
+        prices, divs[['Instrument', 'Date', 'div_amt']],
+        how='outer',
+        on=['Date', 'Instrument']
+    )
+    unadjusted_price_history['div_amt'].fillna(0, inplace=True)
+
+    unadjusted_price_history = pd.merge(
+        unadjusted_price_history, splits,
+        how='outer',
+        on=['Date', 'Instrument']
+    )
+    unadjusted_price_history['split_rto'].fillna(1, inplace=True)
+
+    if unadjusted_price_history.isnull().values.any():
+        raise Exception('missing values detected!')
+
+    return(unadjusted_price_history.to_dict('records'))
+
+@app.callback(
+    Output("returns-tbl-1", "data"),
+    Input("history-tbl-1", "data"),
+    prevent_initial_call = True
+)
+def calculate_returns(history_tbl):
+
+    dt_prc_div_splt = pd.DataFrame(history_tbl)
+
+    # Define what columns contain the Identifier, date, price, div, & split info
+    ins_col = 'Instrument'
+    dte_col = 'Date'
+    prc_col = 'close'
+    div_col = 'div_amt'
+    spt_col = 'split_rto'
+
+    dt_prc_div_splt[dte_col] = pd.to_datetime(dt_prc_div_splt[dte_col])
+    dt_prc_div_splt = dt_prc_div_splt.sort_values([ins_col, dte_col])[
+        [ins_col, dte_col, prc_col, div_col, spt_col]].groupby(ins_col)
+    numerator = dt_prc_div_splt[[dte_col, ins_col, prc_col, div_col]].tail(-1)
+    denominator = dt_prc_div_splt[[prc_col, spt_col]].head(-1)
+
+    return(
+        pd.DataFrame({
+        'Date': numerator[dte_col].reset_index(drop=True),
+        'Instrument': numerator[ins_col].reset_index(drop=True),
+        'rtn': np.log(
+            (numerator[prc_col] + numerator[div_col]).reset_index(drop=True) / (
+                    denominator[prc_col] * denominator[spt_col]
+            ).reset_index(drop=True)
+        )
+    }).pivot_table(
+            values='rtn', index='Date', columns='Instrument'
+        ).to_dict('records')
+    )
+
+
+@app.callback(
     Output("ab-plot", "figure"),
-    Input("returns-tbl", "data"),
-    [State('benchmark-id', 'value'), State('asset-id', 'value')],
+    Input("returns-tbl-1", "data"),
+    [State('benchmark-id-1', 'value'), State('asset-id-1', 'value')],
     prevent_initial_call = True
 )
 def render_ab_plot(returns, benchmark_id, asset_id):
@@ -185,5 +416,21 @@ def render_ab_plot(returns, benchmark_id, asset_id):
         px.scatter(returns, x=benchmark_id, y=asset_id, trendline='ols')
     )
 
+
+@app.callback(
+    Output("alpha_beta_output", "children"),
+    Input("returns-tbl-1", "data"),
+    [State('benchmark-id-1', 'value'), State('asset-id-1', 'value')],
+    prevent_initial_call = True
+)
+def alpha_beta(returns, benchmark_id, asset_id):
+    plt = px.scatter(returns, x=benchmark_id, y=asset_id, trendline='ols')
+    mdl = px.get_trendline_results(plt)
+    alpha = mdl.iloc[0]["px_fit_results"].params[0]
+    beta = mdl.iloc[0]["px_fit_results"].params[1]
+    return f"Alpha: {alpha} | Beta: {beta}"
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
